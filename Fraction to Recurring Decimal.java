47a3e529c9dd078021253a2c1693b80c3be83395
lidong2631 <lidong2631@gmail.com>
2015-03-25 17:35:35 -0400

    leetcode0325

--- Fraction to Recurring Decimal.java
+++ Fraction to Recurring Decimal.java
@@ -1,5 +1,109 @@
 public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+            return "0";
+        if(denominator==0)
+            return "";
+        
+        String res = "";
+        if((numerator<0)^(denominator<0))
+            res += "-";
+        
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        
+        long integer_part = num/den;
+        res += String.valueOf(integer_part);
+        
+        long remainder = (num%den)*10;
+        if(remainder==0)
+            return res;
+        
+        res+=".";
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        while(remainder!=0) {
+            if(map.containsKey(remainder)) {
+                String partA = res.substring(0, map.get(remainder));
+                String partB = res.substring(map.get(remainder), res.length());
+                return partA + "(" + partB + ")";
+            }
+            map.put(remainder, res.length());
+            long result = remainder/den;
+            res += String.valueOf(result);
+            remainder = (remainder%den)*10;
+        }
+        return res;
+    }
+}
+
+自己写的 注意一旦有long类型的数字计算 其结果也应该是long类型 不然会出现如下错误
+
+Line 15: error: incompatible types: possible lossy conversion from long to int
+
+另外复习 原码 反码 补码 http://blog.csdn.net/zq602316498/article/details/39404043
+
+http://blog.csdn.net/ljiabin/article/details/42025037
+
+http://www.meetqun.com/thread-3328-1-1.html
+
+补充下 remainder在map里可以是int 因为余数达不到最大绝对值的整数， 但是因为reminder要＊10，所以map的key用int 而reminder还应该用long long
+
+时间O(n) 空间O(n)
+
+
+
+
+
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+            return "0";
+        if(denominator==0)
+            return "";
+        
+        StringBuffer res = new StringBuffer();
+        if((numerator<0)^(denominator<0))
+            res.append('-');
+        
+        long num = Math.abs((long)numerator);
+        long den = Math.abs((long)denominator);
+        
+        long integer_part = num/den;
+        res.append(integer_part);
+        
+        long remainder = (num%den)*10;
+        if(remainder==0)
+            return res.toString();
+        
+        res.append('.');
+        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
+        while(remainder!=0) {
+            if(map.containsKey(remainder)) {
+                String partA = res.substring(0, map.get(remainder)).toString();
+                String partB = res.substring(map.get(remainder), res.length()).toString();
+                return partA + "(" + partB + ")";
+            }
+            map.put(remainder, res.length());
+            long result = remainder/den;
+            res.append(result);
+            remainder = (remainder%den)*10;
+        }
+        return res.toString();
+    }
+}
+
+根据上面的解法改用StringBuffer 可以节省空间
+
+
+
+
+
+
+
+
+
+public class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
         if(numerator==0)        //被除数为0情况
             return "0";
         if(denominator==0)      //除数为0
@@ -37,8 +141,3 @@ public class Solution {
 
 这题很绕 要好好捉摸 几个问题
 1 注意java中整数的存储 正整数 0~2^31 负整数-2^32~-1
-
-
-http://blog.csdn.net/ljiabin/article/details/42025037
-
-http://www.meetqun.com/thread-3328-1-1.html
--- Repeated DNA Sequences.java
+++ Repeated DNA Sequences.java
@@ -38,20 +38,20 @@ from meetqun
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
         List<String> res = new ArrayList<String>();
-        if(s==null || s.length()<10)
+        if(s==null || s.length()<10)        //长度小于10就直接返回
             return res;
-        int mask = 0x3FFFF;
-        int sequence = 0;
+        int mask = 0x3FFFF; //这里mask一共18位 是因为DNA序列有ACGT四种字符 可以用2bits表示一个字符 每次我们都用10个字符的sequence的后9位
+        int sequence = 0;   //与新进入的一个字符组合 所以这里掩码想取9个字符*2bits 就是18位即0x3FFFF
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        sequence = toInt(s.substring(0, 10));
+        sequence = toInt(s.substring(0, 10));   //toInt将一个string转成我们要的二进制序列
         map.put(sequence, 1);
         
-        for(int i=1; i<s.length()-9; i++) {
-            sequence = (sequence&mask)<<2|toInt(s.charAt(i+9));
-            if(map.containsKey(sequence)) {
+        for(int i=1; i<s.length()-9; i++) { //循环从第11个字符开始取 到字符串结束
+            sequence = (sequence&mask)<<2|toInt(s.charAt(i+9)); //每次sequence左移2位加入新进来的一个字符 组成新的sequence
+            if(map.containsKey(sequence)) {     //如果字典里有 将这个sequence加入最终哦给你结果 否则将这个sequence加入字典
                 if(map.get(sequence)==1)
                     res.add(s.substring(i, i+10));
-                map.put(sequence, map.get(sequence)+1);
+                map.put(sequence, map.get(sequence)+1); //这里是为防止多次重复产生重复结果
             }
             else
                 map.put(sequence, 1);
@@ -79,3 +79,7 @@ public class Solution {
             return 3;
     }
 }
+
+时间O(n)  ?空间最坏O(n^2)  这题主要考核位运算 要注意凡是一定范围的字符都可以考虑转换成二进制表示 
+
+这题可以扩展为不止4个字符 k个字符
