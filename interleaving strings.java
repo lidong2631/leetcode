e1c03c8f6f0f3cc6318c4407094a0173fe4fe632
dongli <lidong2631@gmail.com>
2015-04-30 00:17:14 -0400

    leetcode0430

--- interleaving strings.java
+++ interleaving strings.java
@@ -25,6 +25,31 @@ Note: 这个版本跟pyhton版一样 只是下标是按code ganker的程序取�
 
 
 
+public class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s1.length()+s2.length()!=s3.length())
+            return false;
+        String minWord = s1.length()>s2.length()? s2:s1;    //这里要注意 不能写成s1.length()<s2.length()?s1:s2 碰到s1长度跟s2相等会有问题
+        String maxWord = s1.length()>s2.length()? s1:s2;
+        boolean[] res = new boolean[minWord.length()+1];
+        res[0] = true;
+        for(int i=0; i<minWord.length(); i++) {
+            res[i+1] = res[i]&&minWord.charAt(i)==s3.charAt(i);
+        }
+        for(int i=0; i<maxWord.length(); i++) {
+            res[0] = res[0]&&maxWord.charAt(i)==s3.charAt(i);
+            for(int j=0; j<minWord.length(); j++) {
+                res[j+1] = res[j+1]&&maxWord.charAt(i)==s3.charAt(i+j+1) || res[j]&&minWord.charAt(j)==s3.charAt(i+j+1);
+            }
+        }
+        return res[minWord.length()];
+    }
+}
+
+时间O(m*n) O(min(m, n))
+
+
+
 
 
 
