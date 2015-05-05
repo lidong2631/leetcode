dcab17dc0268a1df0d6f286cb74c735b6e57e438
lidong2631 <lidong2631@gmail.com>
2015-05-05 17:45:39 -0400

    leetcode0505

--- Reverse Linked List.java
+++ Reverse Linked List.java
@@ -1,4 +1,39 @@
 Iterative
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode second = head.next;
+        ListNode third = head.next.next;
+        
+        second.next = head;
+        head.next = null;
+        if(third==null)
+            return second;
+        ListNode curr = third; ListNode prev = second;
+        while(curr!=null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+
+from programmerInterview O(n) O(1)
+
+
+
+
 
 public void reverse(ListNode head) {
     if(head==null || head.next==null)
@@ -25,7 +60,42 @@ public void reverse(ListNode head) {
 
 
 
+
+
 Recursive
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode curr = head;
+        List<ListNode> res = new ArrayList<ListNode>();
+        helper(curr, res);
+        return res.get(0);
+    }
+    
+    private void helper(ListNode curr, List<ListNode> res) {
+        if(curr.next==null) {
+            res.add(curr);
+            return;
+        }
+        helper(curr.next, res);
+        curr.next.next = curr;
+        curr.next = null;
+    }
+}
+
+
+
+
+
 
 public void reverse(ListNode curr) {
     if(curr==null)  //check for empty list
