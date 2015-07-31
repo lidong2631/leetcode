9af2867c09e07838c7e2f272108f2323d060e167
dongli <lidong2631@gmail.com>
2015-07-30 23:43:23 -0400

    leetcode0730

--- Kth Smallest Element in a BST.java
+++ Kth Smallest Element in a BST.java
@@ -9,10 +9,10 @@
  */
 public class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        int count = countNode(root.left);
-        if(k<=count) {
+        int count = countNode(root.left);   //计算左子树节点个数
+        if(k<=count) {  //如果k小于等于count 第k小值在左子树
             return kthSmallest(root.left, k);
-        } else if(k>count+1) {
+        } else if(k>count+1) {  //否则在右子树
             return kthSmallest(root.right, k-1-count);  
         }
         return root.val;
@@ -25,6 +25,52 @@ public class Solution {
     }
 }
 
+dfs O(nlogn) O(1)
+
 The best performance is we just have to count the nodes for once (i.e. kth is root), which is O(n); 
 the worse/average case when we need count nodes for each subtree traversal, binary search is always log(n), 
 and number of traversed subtrees could be n, then as total is O(nlog(n)).
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(root!=null) {
+            stack.push(root);
+            root = root.left;
+        }
+        while(k!=0) {
+            TreeNode curr = stack.pop();
+            k--;
+            if(k==0)
+                return curr.val;
+            curr = curr.right;
+            while(curr!=null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+        }
+        return -1;
+    }
+}
+利用中序遍历 树的中序遍历迭代法 利用stack
+
+O(n) O(logn)
+
+
+https://leetcode.com/discuss/43771/implemented-java-binary-search-order-iterative-recursive
+
+两种解法 1 In-Order Traversal 2 Binary Search
+http://www.geeksforgeeks.org/find-k-th-smallest-element-in-bst-order-statistics-in-bst/
