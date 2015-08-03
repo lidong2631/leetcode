5946692de74842f714e456b53377a2f66d4bb86b
lidong2631 <lidong2631@gmail.com>
2015-08-03 16:33:19 -0400

    leetcode0803

--- implement strStr(java).py
+++ implement strStr(java).py
@@ -129,6 +129,31 @@ public class Solution {
 
 
 
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0)
+            return 0;
+        if(needle.length()>haystack.length())
+            return -1;
+        for(int i=0; i<=haystack.length()-needle.length(); i++) {
+            boolean flag = true;
+            for(int j=0; j<needle.length(); j++) {
+                if(haystack.charAt(i+j)!=needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                return i;
+        }
+        return -1;
+    }
+}
+
+
+
+
 public class Solution {
     public String strStr(String haystack, String needle) {
         if(haystack==null || needle==null || needle.length()==0)
