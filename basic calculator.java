0c6434c44f360798132a771b7750edf24498a76c
lidong2631 <lidong2631@gmail.com>
2015-08-28 17:02:56 -0400

    leetcode0828

--- basic calculator.java
+++ basic calculator.java
@@ -46,6 +46,190 @@ https://leetcode.com/discuss/39481/concise-solution-using-recursion-postfix-conv
 
 
 
+
+public class Solution {
+    public int calculate(String s) {
+        return evalRPN(InfixToPostFix(s));
+    }
+    
+    private int rank(char op) {
+
+        switch(op) {
+            case '+':
+                return 1;
+            case '-':
+                return 1;
+            case '*':
+                return 2;
+            case '/':
+                return 2;
+            default:
+                return 0;   //'('
+        }
+    }
+    private List<String> InfixToPostFix(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        List<String> res = new LinkedList<>();
+    
+        int num = 0;
+        boolean isOperand = false;
+        for(char c : s.toCharArray()) {
+            if(c>='0' && c<='9') {
+                num = 10*num + c-'0';
+                isOperand = true;
+            }
+            else {
+                if(isOperand)
+                    res.add(String.valueOf(num));
+                num = 0;
+                isOperand = false;
+    
+                if(c==' ' || c=='\t')
+                    continue;
+    
+                if(c=='(') {
+                    stack.push('(');
+                }
+                else if(c==')') {
+                    while(stack.peek()!='(')
+                        res.add(String.valueOf(stack.pop()));
+                    stack.pop();  //pop '('
+                }
+                else {
+                    while(!stack.empty() && rank(stack.peek())>=rank(c))
+                        res.add(String.valueOf(stack.pop()));
+                    stack.push(c);
+                }
+            }
+        }
+        if(isOperand)
+            res.add(String.valueOf(num));
+        while(!stack.empty())
+            res.add(String.valueOf(stack.pop()));
+        return res;
+    }
+    
+    private int evalRPN(List<String> tokens) {
+        if(tokens == null || tokens.size() == 0)
+        {
+            return 0;
+        }
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        for(int i=0; i<tokens.size(); i++)
+        {
+            if(tokens.get(i).equals("+")) 
+                stack.push(stack.pop()+stack.pop());
+            
+            else if(tokens.get(i).equals("-"))
+                stack.push(-stack.pop()+stack.pop());
+            else if(tokens.get(i).equals("*"))
+                stack.push(stack.pop()*stack.pop());
+                
+            else if(tokens.get(i).equals("/"))
+            {
+                int op2 = stack.pop();
+                int op1 = stack.pop();
+                stack.push(op1/op2);
+            }
+            
+            else
+                stack.push(Integer.parseInt(tokens.get(i)));
+        }
+        return stack.pop();
+    }
+}
+
+超时
+
+注意理解infix to postfix的过程
+http://scriptasylum.com/tutorials/infix_postfix/algorithms/infix-postfix/
+
+
+
+int rank(char op) {
+
+    switch(op) {
+        case '+':
+            return 1;
+        case '-':
+            return 1;
+        case '*':
+            return 2;
+        case '/':
+            return 2;
+        default:
+            return 0;   //'('
+    }
+}
+
+
+List< InfixToPostFix(String s) {
+    Stack<Character> stack = new Stack<Character>();
+    List<> res = new LinkedList<>();
+
+    int num = 0;
+    boolean isOperand = false;
+    for(char c : s.toCharArray()) {
+        if(c>='0' && c<='9') {
+            num = 10*num + c-'0';
+            isOperand = true;
+        }
+        else {
+            if(isOperand)
+                res.add(num);
+            num = 0;
+            isOperand = false;
+
+            if(c==' ' || c=='\t')
+                continue;
+
+            if(c=='(') {
+                stack.push('(');
+            }
+            else if(c==')') {
+                while(!stack.peek()!='(')
+                    res.add(stack.pop());
+                res.pop();  //pop '('
+            }
+            else {
+                while(!stack.empty() && rank(stack.peek())>=rank(c))
+                    res.add(stack.pop());
+                stack.push(c);
+            }
+        }
+    }
+    if(isOperand)
+        res.add(num);
+    while(!stack.empty())
+        res.add(stack.pop());
+    return res;
+}
+
+int evaluatePostfix(List<Object> postfix) {
+    Stack<Integer> operands = new Stack<Integer>();
+    int a = 0, b = 0;
+    for (Object s : postfix) {
+        if(s instanceof Character){
+            char c = (Character) s;
+            b = operands.pop();
+            a = operands.pop();
+            switch (c) {
+                case '+': operands.push(a + b); break;
+                case '-': operands.push(a - b); break;
+                case '*': operands.push(a * b); break;
+                default : operands.push(a / b); 
+            }
+        }else { // instanceof Integer
+            operands.push((Integer)s);
+        }
+    }
+    return operands.pop();
+}
+
+public int calculate(String s) {
+    return evaluatePostfix(infixToPostfix(s));
+}
+
 先转postfix notation 再利用evaluate reverse polish那题计算结果
 
 https://leetcode.com/discuss/39454/accepted-infix-postfix-based-solution-explaination-600ms
