d012a723a50f6fe8945d4ddeec079aadc136fda3
dongli <lidong2631@gmail.com>
2016-08-20 13:33:01 -0400

    lc0820

--- Course Schedule.java
+++ Course Schedule.java
@@ -1,39 +1,29 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites==null || prerequisites.length==0 || prerequisites[0].length==0)
-            return true;
-        boolean[] visited = new boolean[numCourses];    //dfs中标记节点是否访问过 一旦设为true就不会再变回来
-        boolean[] recStack = new boolean[numCourses];   //标记当前递归栈中该节点是否已访问过 
-                                                    //如果当前一轮递归访问完没有cycle 会重新设为false
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        for(int i=0; i<numCourses; i++)       //先构建一个adjacency list
-            res.add(new ArrayList<Integer>());
-        for(int i=0; i<prerequisites.length; i++) {
-            for(int j=1; j<prerequisites[i].length; j++)
-                res.get(prerequisites[i][0]).add(prerequisites[i][j]);
-        }
-        
-        for(int i=0; i<numCourses; i++) {       //dfs
-            if(helper(i, visited, recStack, res))   //helper用来detect cycle return true if there is a cycle
-                return false;
+        List<List<Integer>> adjList = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; i++)
+            adjList.add(new ArrayList<>());
+        for (int[] p : prerequisites)
+            adjList.get(p[0]).add(p[1]);
+        boolean[] visited = new boolean[numCourses];
+        boolean[] recStack = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (helper(visited, recStack, i, adjList)) return false;
         }
         return true;
     }
     
-    private boolean helper(int v, boolean[] visited, boolean[] recStack, List<List<Integer>> res) {
-        if(!visited[v]) {
-            visited[v] = true;
-            recStack[v] = true;
-            
-            List<Integer> item = res.get(v);    //拿到当前点的邻居list
-            for(int i=0; i<item.size(); i++) {
-                if(!visited[item.get(i)] && helper(item.get(i), visited, recStack, res))    //dfs将遇到的节点visited和recstack都设为true
-                    return true;
-                else if(recStack[item.get(i)])  //如果已经访问过这个邻居节点并且其rec记录为true 说明有cycle
-                    return true;
+    private boolean helper(boolean[] visited, boolean[] recStack, int i, List<List<Integer>> adjList) {
+        if (!visited[i]) {
+            visited[i] = true;
+            recStack[i] = true;
+            List<Integer> list = adjList.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                if (!visited[list.get(j)] && helper(visited, recStack, list.get(j), adjList)) return true;
+                else if (recStack[list.get(j)]) return true;
             }
         }
-        recStack[v] = false;    //如果遍历完当前点所有邻居都没有cycle 要将rec重新设为false
+        recStack[i] = false;        // careful this need to be executed outside if
         return false;
     }
 }
